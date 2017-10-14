767b42e8a16b33a0b519459cbd8ee772abe92b48
Admin <admin@A51-09.local>
2017-10-14 14:59:07 -0500

    Clase07

--- Angular/Clase07/appDirectivas/src/app/select.directive.ts
+++ Angular/Clase07/appDirectivas/src/app/select.directive.ts
@@ -1,11 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appSelect]'
 })
-export class SelectDirective {
+export class SelectDirective implements OnInit {
+  @Input("appSelect") lista: Array<string>
 
+  constructor(private elementRef: ElementRef) { }
 
-  constructor() { }
+  ngOnInit(){
+    this.lista.forEach((item, index) => {
+      const option = document.createElement("option")
+      option.innerHTML = item
+      option.value = `${index}`
+
+      this.elementRef.nativeElement.appendChild(option)
+    })
+  }
 
 }
