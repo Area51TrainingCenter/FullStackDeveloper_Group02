f68c472146599ef8e7480c8f06a7082072b5b9f1
Alumno <alumno@A51MM-14.local>
2017-11-11 10:30:49 -0500

    Clase11

---
+++ Angular/Clase11/appHttp/src/app/seguridad/autenticacion.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { IUsuario } from '../nucleo/interfaces';
+import { AuthApiService } from './auth-api.service';
+import { Observable } from 'rxjs/Observable';
+import { HttpResponse } from '@angular/common/http';
+import { Subject } from 'rxjs/Subject';
+
+@Injectable()
+export class AutenticacionService {
+  private usuario: IUsuario
+  private autenticado: boolean = false
+
+  cambioEstado = new Subject()
+
+  constructor(private authApiService: AuthApiService) { }
+
+  logout(){
+
+  }
+
+  registro(usuario: IUsuario): Observable<IUsuario> {
+    return this.authApiService.registro(usuario)
+  }
+
+  login(usuario: IUsuario){
+    return this.authApiService.login(usuario)
+      .subscribe(
+        (usuario: IUsuario) => {
+          this.autenticado = true
+          this.usuario = usuario
+          localStorage.setItem("token", usuario.token)
+          this.cambioEstado.next(true)
+        },
+        (error: Error) => console.log(error.message)
+      )
+  }
+
+  obtenerToken(){
+
+  }
+
+  estaAutenticado():boolean {
+    return this.autenticado
+  }
+
+}
