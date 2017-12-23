6bfe4027fd6e56af69309514a2c87e8ebaa1c72d
Admin <admin@area51.com>
2017-12-23 16:04:51 -0500

    Clase16

--- Node/Clase16/socketio/src/servidor.ts
+++ Node/Clase16/socketio/src/servidor.ts
@@ -1,6 +1,7 @@
 // Declaraciones e importaciones
 import express = require("express")
 import {Application, Request, Response, NextFunction} from "express"
+import socket = require("socket.io")
 
 // Configuraciones
 const app: Application = express()
@@ -21,3 +22,30 @@ const servidor = app.listen(4000, ()=> {
 })
 
 // Socket.io
+const io = socket(servidor)
+
+const usuarios = []
+
+io.on("connection", socket => {
+	console.log("Conexión detectada", socket.id)
+
+	usuarios.push({
+		socket: socket,
+		datosUsuario: {}
+	})
+
+	socket.on("mensaje del chat", data => {
+		console.log(data)
+		//socket.emit("enviando", data)
+		//socket.broadcast.emit("enviando", data)
+		io.emit("enviando", data)
+	})
+
+	socket.on("feedback", data => {
+		socket.broadcast.emit("escribiendo", data)
+	})
+
+	io.on("disconnect", socket => {
+
+	})
+})
